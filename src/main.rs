2f6e206adc18e96344f974dadeb93d4944f88768
Jesse Gibson <JesseTheGibson@gmail.com>
2019-09-20 23:13:36 -0600

    Add a boilerplate CLI app
    I'll probably start with ls-remote.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use clap::{App, AppSettings, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("arc")
+        .version(env!("CARGO_PKG_VERSION"))
+        .about("Node version manager")
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .subcommand(SubCommand::with_name("ls-remote"))
+        .get_matches();
+
+    match matches.subcommand() {
+        ("ls-remote", Some(_)) => unimplemented!(),
+        _ => unreachable!(),
+    }
 }
